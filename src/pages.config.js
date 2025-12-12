10a32ccba672fc8e262954111a6413be5a02d383
doditz <sebastienbrulotte@gmail.com>
2025-12-12 22:31:30 +0000

    Phase 2: DSTIB-Hebden intégré

--- src/pages.config.js
+++ src/pages.config.js
@@ -27,7 +27,6 @@ import DevTestAnalytics from './pages/DevTestAnalytics';
 import CollaborativeWorkspace from './pages/CollaborativeWorkspace';
 import NeuronasGauntlet from './pages/NeuronasGauntlet';
 import MemoryExplorer from './pages/MemoryExplorer';
-import PipelineTest from './pages/PipelineTest';
 import __Layout from './Layout.jsx';
 
 
@@ -61,7 +60,6 @@ export const PAGES = {
     "CollaborativeWorkspace": CollaborativeWorkspace,
     "NeuronasGauntlet": NeuronasGauntlet,
     "MemoryExplorer": MemoryExplorer,
-    "PipelineTest": PipelineTest,
 }
 
 export const pagesConfig = {
